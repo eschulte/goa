cd5b64bfdbb3a1aa9407310b954d1ee2fcb42e2b
Eric Schulte <eric.schulte@gmx.com>
2012-10-30 17:11:59 -0600

    running optimization locally

--- src/optimize.scm
+++ src/optimize.scm
@@ -11,38 +11,47 @@
 
 ;;; Code:
 (use-modules
- (srfi srfi-1) (srfi srfi-11) (srfi srfi-88)
+ (srfi srfi-1) (srfi srfi-11) (srfi srfi-69) (srfi srfi-88)
  (ice-9 match) (ice-9 format)
  (sevo utility) (sevo sevo))
 
-(define max-fit #f)
 (define num-threads #f)
 (define program "blackscholes")
 (define source (from-file "../data/blackscholes.c"))
 (define original '((edit-history source)))
 
 (define evaluate
-  (memoize
-   (lambda (variant)
-     (apply values
-      (with-temp-file-of (path "/tmp/clang-mutate-" ".c" (genome variant))
-        (call-with-values
-            (lambda () (command-to-string "../bin/host-test" program path))
-          (lambda (stdout err)
-            (list
-             (map (lambda (line)
-                    (let ((split (delete "" (string-split line #\space))))
-                      (cons (string->keyword (car split))
-                            (map (lambda (cell) (or (string->number cell) cell))
-                                 (cdr split)))))
-                  (delete "" (string-split stdout #\newline)))
-             err))))))))
+  (memoize 'evaluate
+    (lambda (variant)
+      (apply values
+        (with-temp-file-of (path "/tmp/clang-mutate-" ".c" (genome variant))
+          (call-with-values
+              (lambda () (command-to-string "../bin/host-test" program path))
+            (lambda (stdout err)
+              (list
+               (map (lambda (line)
+                      (let ((split (remove
+                                    (lambda (el)
+                                      (member el (list "" "\n" "\t" "\r")))
+                                    (string-split line #\space))))
+                        (cons (string->keyword (car split))
+                              (map (lambda (cell) (or (string->number cell) cell))
+                                   (cdr split)))))
+                    (delete "" (string-split stdout #\newline)))
+               err))))))))
 
 (define (multi-obj-fitness variant)
   "Calculate the total combined fitness of PHENOME based on `evaluate' output."
   (let-values (((metrics err) (evaluate variant)))
-    (if (not (zero? err)) 0
-        (/ 1 (apply max (assoc-ref metrics #:completion-time))))))
+    (let ((vals (assoc-ref metrics #:completion-time)))
+      (if (not (zero? err)) 0
+          (if (all number? vals)
+              (/ 1 (apply max vals))
+              (begin (format #t "bad for completion-time:~a~%~a"
+                             vals variant)
+                     0))))))
 
-(evolve (repeatedly 10 (rand-mutate 0.8 original)) multi-obj-fitness max-fit
-        #:num-threads num-threads)
+(evolve (repeatedly 20 (rand-mutate 0.8 original)) multi-obj-fitness
+        #:max-gen 2)
+
+(write-memoized "run.cache" #:overwrite #t)
