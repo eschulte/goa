c50272d476f239a7d6e2d610c65d92f24701e799
Eric Schulte <schulte.eric@gmail.com>
2013-02-27 12:14:20 -0700

    beginning to calculate energy-delay product

--- src/blackscholes.lisp
+++ src/blackscholes.lisp
@@ -6,6 +6,12 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (enable-curry-compose-reader-macros))
 
+(defvar infinity
+  #+sbcl
+  SB-EXT:DOUBLE-FLOAT-POSITIVE-INFINITY
+  #-(or sbcl)
+  (error "must specify a positive infinity value"))
+
 (defvar *work-dir* "sh-runner/work/")
 
 (defvar *test* "../../bin/blackscholes-test")
@@ -25,6 +31,58 @@
 (defvar *neutral-walk* (list (list (edits *orig*)))
   "Variable to hold the results of the walk.")
 
+(defvar *stdout*)
+
+(defun parse-stdout (stdout)
+  "Parse the Graphite output of host-test."
+  (remove
+      nil
+      (mapcar
+       (lambda (line)
+         (let ((fields (split-sequence #\Space (regex-replace-all "" line "")
+                                       :remove-empty-subseqs t)))
+           (unless (null fields)
+             (cons (make-keyword (string-upcase (car fields)))
+                   (mapcar (lambda (c) (or (ignore-errors (parse-number c)) c))
+                           (cdr fields))))))
+       (remove-if (lambda (line) (or (scan "hooks" line)
+                                (scan "warning" line)
+                                (scan "spawn_master" line)))
+                  (split-sequence #\Newline stdout :remove-empty-subseqs t)))))
+
+(defun group-stats (stats &aux group results)
+  (dolist (row stats (reverse (cons (reverse group) results)))
+    (case (car row)
+      ((:tile-summary :core-model-summary :network-summary :cache-summary)
+       (setf group nil))
+      ((:cache-l1-i :cache-l2 :cache-l1-d :dram-performance-model-summary)
+       (when group (push (reverse group) results))
+       (setf group row))
+      (:network-model
+       (when group
+         (push (reverse group) results)
+         (setf group nil))
+       (push row group))
+      (t
+       (if group
+           (push row group)
+           (push row results))))))
+
+(defun energy-delay-product (stats)
+  (flet ((energy (group)
+           (+ (reduce #'+ (cdr (assoc :static-power group)))
+              (reduce #'+ (cdr (assoc :dynamic-energy group))))))
+    (* 
+     ;; Runtime
+     (reduce #'+ (aget :completion-time (group-stats (parse-stdout *stdout*))))
+     ;; Energy
+     (reduce #'+ (mapcar #'energy
+                         (cons
+                          (aget '(:network-model 2) stats :test #'tree-equal)
+                          (mapcar {aget _ stats}
+                                  '(:cache-l1-i :cache-l1-d :cache-l2
+                                    :dram-performance-model-summary))))))))
+
 (defun test (variant)
   (with-temp-file (file)
     (phenome variant :bin file)
@@ -35,6 +93,13 @@
           0))))
 (memoize #'test :key [#'edits #'car])
 ;; (un-memoize 'test)
+
+(defun graphite-test (variant)
+  (multiple-value-bind (stdout stderr errno) (shell "cat /tmp/output")
+    (declare (ignorable stderr))
+    (if (zerop errno)
+        (energy-delay-product (group-stats (parse-stdout stdout)))
+        infinity)))
 
 (defun neutralp (variant)
   (setf (fitness variant) (test variant))
