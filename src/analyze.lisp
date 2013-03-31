e52085179ba9f487b39bee8efb14af2d0545497e
Eric Schulte <schulte.eric@gmail.com>
2013-03-31 09:44:01 -0600

    analyzing results of t4 run

--- src/analyze.lisp
+++ src/analyze.lisp
@@ -14,20 +14,7 @@
 
 (defvar *stats* nil "List of statistics read from a run.")
 
-(defvar *pops* nil "Hold all pops from a run.")
-
-(defvar *evals*
-  '(256 512 768 1024 1280 1536 1792 2048 2304 2560 2816 3072 3328 3584 3840 4096
-    4352 4608 4864 5120 5376 5632 5888 6144 6400 6656 6912 7168 7424 7680 7936
-    8192 8448 8704 8960 9216 9472 9728 9984 10240 10496 10752 11008 11264 11520
-    11776 12032 12288 12544 12800 13056 13312 13568 13824 14080 14336 14592
-    14848 15104 15360 15616 15872 16128 16384 16640 16896 17152 17408 17664
-    17920 18176 18432 18688))
-
-(loop :for eval :in *evals* :do
-   (let ((pop (restore (format nil "results/bs-evo/pop-~d.store" eval))))
-     (mapcar #'bs-opt::clean-stats pop)
-     (push (cons eval pop) *pops*)))
+(defvar *run* nil "Hold all pops from a run.")
 
 (defun stat-summary (sample)
   (mapcar [#'float {funcall _ sample}]
@@ -40,11 +27,41 @@
                 {percentile _ 75}
                 {apply #'max})))
 
-;; print stats across all populations
-(with-open-file (out "/tmp/err.data" :direction :output)
-  (loop :for eval :in *evals* :do
-     (format out "~a ~{~a~^ ~}~%" eval
-             (stat-summary (remove bs-opt::infinity
-                             (remove nil
-                               (mapcar [{bs-opt::aget 'bs-opt::error} #'bs-opt::stats]
-                                       (cdr (assoc eval *pops*)))))))))
+(defun pop-means (stat)
+  (mapcar [#'mean {mapcar {aget stat}} #'cdr] (sort *run* #'< :key #'car)))
+
+(defun stat-to-file (stat file)
+  (with-open-file (out file :direction :output)
+    (mapc [{format out "~a~%"} #'float] (pop-means stat))))
+
+#+laptop-analysis
+(progn
+(cl-fad:walk-directory "results/bs-evo-t4/"
+   (lambda (file)
+     (multiple-value-bind (whole matches)
+         (scan-to-strings "pop-([0-9]+)-stats" (pathname-name file))
+       (unless (null matches)
+         (push (cons (parse-number (aref matches 0)) (restore file))
+               *run*)))))
+
+(loop :for eval :in *evals* :do
+   (let ((pop (restore (format nil "results/bs-evo/pop-~d.store" eval))))
+     (mapcar #'bs-opt::clean-stats pop)
+     (push (cons eval pop) *pops*)))
+
+(mapc {apply #'stat-to-file}
+      '((:fit   "results/t4-fit.data")
+        (:size  "results/t4-size.data")
+        (:error "results/t4-error.data")
+        (:instr "results/t4-instr.data")))
+
+;; most fit at beginning of run
+(extremum (cdar *run*) #'< :key {aget :fit})
+;; ((:FIT . 1.4960707e8) (:EDITS . 2) (:SIZE . 1103) (:ERROR . 0) (:INSTR . 149605972))
+
+;; most fit at end of run
+(extremum (cdar (last *run*)) #'< :key {aget :fit})
+;; ((:FIT . 6281291.5) (:EDITS . 6) (:SIZE . 1109) (:ERROR . 4.75942) (:INSTR . 5939967))
+
+(caar (last *run*)) ;; => 254135
+)
