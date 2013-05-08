7c826f29a974935b37dbc135bee599c791759c32
Eric Schulte <schulte.eric@gmail.com>
2013-05-08 17:07:40 -0400

    tweaks

--- src/optimize.lisp
+++ src/optimize.lisp
@@ -13,7 +13,8 @@
 (mapcar #'require '(:software-evolution :cl-store :split-sequence :cl-ppcre))
 (defpackage :optimize
   (:use :common-lisp :software-evolution :software-evolution-utility
-        :alexandria :metabang-bind :curry-compose-reader-macros)
+        :alexandria :metabang-bind :curry-compose-reader-macros
+        :cl-store :split-sequence :cl-ppcre)
   (:shadow :type :magic-number))
 (in-package :optimize)
 (eval-when (:compile-toplevel :load-toplevel :execute)
@@ -44,10 +45,13 @@
 (defvar *orig*   nil "Original version of the program to be run.")
 (defvar *test-fmt* nil "Set to the string used to run the test shell script.")
 (defvar *function* nil "Fitness function.")
-(defvar *threads*  4   "Number of cores to use.")
+(defvar *threads*  1   "Number of cores to use.")
 (defvar *evals* (expt 2 20) "Maximum number of test evaluations.")
 (defvar *max-err* 0 "Maximum allowed error.")
-(defvar *model* nil "HW counter model to optimized.")
+(defvar *model* intel-energy-model "HW counter model to optimized.")
+(setf *max-population-size* (expt 2 9)) ;; Default max pop size
+(setf *fitness-predicate* #'<)
+(setf *tournament-size* 4)
 
 (defun parse-stdout (stdout)
   (mapcar (lambda-bind ((val key))
@@ -62,16 +66,19 @@
 (defun run (asm)
   (with-temp-file (bin)
     (phenome asm :bin bin)
-    (multiple-value-bind (stdout stderr errno) (shell *test-fmt* bin)
+    (note 4 "running ~a~%" asm)
+    (multiple-value-bind (stdout stderr errno) (shell "~a ~a" *script* bin)
       (declare (ignorable stderr))
-      (cons `(:exit . ,errno)
-            (ignore-errors (parse-stdout stdout))))))
+      (cons `(:exit . ,errno) (ignore-errors (parse-stdout stdout))))))
 
 (defun test (asm)
   (or (ignore-errors
-        (unless (stats asm) (setf (stats asm) (run asm)))
+        (unless (stats asm)
+          (note 4 "gathering stats for ~S~%" asm)
+          (setf (stats asm) (run asm)))
+        (note 4 "stats: ~S~%" (stats asm))
         (when (<= (aget :error (stats asm)) *max-err*)
           (let ((stats (stats asm)))
-            (reduce (lambda-bind ((acc (hw . cf))) (+ acc (* cf (aget hw stats))))
+            (reduce (lambda-bind (acc (hw . cf)) (+ acc (* cf (aget hw stats))))
                     *model* :initial-value 0))))
       infinity))
