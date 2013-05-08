f0c09825ffc9fb681adcb982aad5ac46e2e27f10
Eric Schulte <schulte.eric@gmail.com>
2013-05-08 14:29:41 -0400

    more generous limit allows perf to run

--- bin/limit.c
+++ bin/limit.c
@@ -9,9 +9,9 @@ int main(int argc, char *argv[]) {
   struct rlimit limit;
   limit.rlim_cur = 8; limit.rlim_max = 8;
   setrlimit(RLIMIT_CPU, &limit);       // cpu seconds
-  limit.rlim_cur = 1024; limit.rlim_max = 1024;
+  limit.rlim_cur = 16384; limit.rlim_max = 16384;
   setrlimit(RLIMIT_NPROC, &limit);     // number of spawned processes
-  limit.rlim_cur = 1024; limit.rlim_max = 1024;
+  limit.rlim_cur = 16384; limit.rlim_max = 16384;
   setrlimit(RLIMIT_NOFILE, &limit);    // number of open files
   limit.rlim_cur = 17179607040; limit.rlim_max = 17179607040;
   setrlimit(RLIMIT_FSIZE, &limit);     // max file size (bytes)
