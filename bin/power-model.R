ddc6f1646ab3c2c302cb2e258a5c18c7dad40e97
Eric Schulte <schulte.eric@gmail.com>
2013-05-15 10:12:06 -0600

    alternate power model calculation w/R

---
+++ bin/power-model.R
@@ -0,0 +1,29 @@
+#!/usr/bin/Rscript
+#
+# Usage: pweor-model.R FILENAME
+#  build a linear power model from the csv data in FILENAME
+#
+#  model used is
+#
+#   watts = C_cycles × M_cycles +
+#           C_instructions × M_instructions +
+#           C_fops × M_fops +
+#           C_mem × M_mem
+#  where
+#    fops = r533f00 or (r532010 + r538010)
+#     mem = cache.misses + cache.references
+#
+args <- commandArgs(trailingOnly = TRUE);
+
+if (length(args) < 1) # Check that required arguments exist
+{ stop("Pass the path to benchmarks.csv as first argument.") }
+
+metrics <- read.csv(args[1]);
+
+if('r533f00' %in% colnames(metrics))
+{ fops <- metrics$r533f00; } else
+{ fops <- rowSums(data.frame(metrics$r532010, metrics$r538010)); }
+
+mem <- rowSums(data.frame(metrics$cache.references, metrics$cache.misses));
+
+summary(lm(metrics$watts ~ metrics$cycles + metrics$instructions + fops + mem))
