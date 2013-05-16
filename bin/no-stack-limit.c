8597440ee2ca9d35a4ad4266108ffcd9b4f96e0e
Eric Schulte <schulte.eric@gmail.com>
2013-05-15 20:12:17 -0600

    testing swaptions as well as blackscholes

--- bin/no-stack-limit.c
+++ bin/no-stack-limit.c
@@ -7,17 +7,17 @@ int main(int argc, char *argv[]) {
 
   // resource/time limits
   struct rlimit limit;
-  limit.rlim_cur = 8; limit.rlim_max = 8;
+  limit.rlim_cur = 24; limit.rlim_max = 24;
   setrlimit(RLIMIT_CPU, &limit);       // cpu seconds
   limit.rlim_cur = 16384; limit.rlim_max = 16384;
   setrlimit(RLIMIT_NPROC, &limit);     // number of spawned processes
   limit.rlim_cur = 16384; limit.rlim_max = 16384;
   setrlimit(RLIMIT_NOFILE, &limit);    // number of open files
-  limit.rlim_cur = 17179607040; limit.rlim_max = 17179607040;
+  limit.rlim_cur = 549747425280; limit.rlim_max = 549747425280;
   setrlimit(RLIMIT_FSIZE, &limit);     // max file size (bytes)
   setrlimit(RLIMIT_MEMLOCK, &limit);   // max memory locked into RAM (bytes)
 
-  alarm(12);                           // wall clock seconds
+  alarm(24);                           // wall clock seconds
                                        // scaled back w/o graphite
 
   // run
