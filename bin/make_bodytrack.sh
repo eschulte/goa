59d0bb0606726d6691d0fb8a9053ff8a3a71a467
Eric Schulte <schulte.eric@gmail.com>
2013-05-29 10:27:31 -0400

    tweak bodytrack building

--- bin/make_bodytrack.sh
+++ bin/make_bodytrack.sh
@@ -24,8 +24,8 @@ fi
 
 # we rename this file so that it won't be included in the concatenated src.
 mv TrackingModelTBB.cpp TrackingModelTBB.bkup
-
 cat *.c* > bodytrack.cpp
+mv TrackingModelTBB.bkup TrackingModelTBB.cpp
 
 # now we undef USE_TBB to controll the includes and comment out duplicate template defs.
 sed -i '1i#undef USE_TBB' bodytrack.cpp
@@ -34,6 +34,4 @@ sed -i '1466a*/' bodytrack.cpp
 sed -i '1771i/*' bodytrack.cpp
 sed -i '1778a*/' bodytrack.cpp
 
-g++ -DHAVE_CONFIG_H -I. -I/nfs/adaptive/stharding/tmp/parsec-3.0/pkgs/apps/bodytrack/src/FlexImageLib -I.. -O3 -g -funroll-loops -fprefetch-loop-arrays -fpermissive -fno-exceptions  -static-libgcc -Wl,--hash-style=both,--as-needed -DPARSEC_VERSION=3.0-beta-20120904 -fexceptions  -MT FlexImage.lo -MD -MP -MF ../../obj/amd64-linux.gcc/FlexImageLib/.deps/FlexImage.Plo -I ../../obj/amd64-linux.gcc/ -L ../../obj/amd64-linux.gcc/FlexImageLib/.libs/ -I ../../../../libs/tbblib/src/include/ -L../../../../libs/tbblib/src/include/ -L ../../obj/amd64-linux.gcc/TrackingBenchmark/threads/.libs/ -S bodytrack.cpp -o bodytrack.s -lflex -lpthread -lthreads
-
-mv TrackingModelTBB.bkup TrackingModelTBB.cpp
+g++ -DHAVE_CONFIG_H -I. -I../FlexImageLib -I.. -O3 -g -funroll-loops -fprefetch-loop-arrays -fpermissive -fno-exceptions  -static-libgcc -Wl,--hash-style=both,--as-needed -DPARSEC_VERSION=3.0-beta-20120904 -fexceptions  -MT FlexImage.lo -MD -MP -MF ../../obj/amd64-linux.gcc/FlexImageLib/.deps/FlexImage.Plo -I ../../obj/amd64-linux.gcc/ -L ../../obj/amd64-linux.gcc/FlexImageLib/.libs/ -I ../../../../libs/tbblib/src/include/ -L../../../../libs/tbblib/src/include/ -L ../../obj/amd64-linux.gcc/TrackingBenchmark/threads/.libs/ -S bodytrack.cpp -o bodytrack.s -lflex -lpthread -lthreads && echo $(pwd)/bodytrack.s
