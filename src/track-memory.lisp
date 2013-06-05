e982ce80e6b7fb1cc2c65687aec82370969eda44
Eric Schulte <schulte.eric@gmail.com>
2013-06-05 10:58:14 -0600

    another simple track-memory fix

--- src/track-memory.lisp
+++ src/track-memory.lisp
@@ -10,12 +10,11 @@
 
 (defun memory-checkpoint ()
   (flet ((space ()
-           (let ((*standard-output* *note-out*))
-             (room)
-             (format *note-out* "~S~%"
-                     (list *fitness-evals*
-                           (length (to-bytes *consolidated-edits*))
-                           (length (to-bytes (car *population*))))))))
+           (mapc {format _ "~S~%" (list *fitness-evals*
+                                        (length (to-bytes *consolidated-edits*))
+                                        (length (to-bytes (car *population*))))}
+                                        *note-out*)
+           (let ((*standard-output* (car (remove t *note-out*)))) (room))))
     (space) (checkpoint) (space)))
 
 (setf *checkpoint-func* #'memory-checkpoint)
