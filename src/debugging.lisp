2bee0030ff126812c162e68281c955a66dc7f413
Eric Schulte <schulte.eric@gmail.com>
2013-06-10 09:35:19 -0600

    more memory debug

--- src/debugging.lisp
+++ src/debugging.lisp
@@ -30,6 +30,19 @@
    :dynamic)
   asm-counter)
 
+(defun join (alist-a alist-b &key (test #'eql) missing)
+  "Join ALIST-A and ALIST-B into an alist keyed by KEY"
+  (let ((coll (make-hash-table :test test)))
+    (mapc (lambda (a) (push (cons :a (cdr a)) (gethash (car a) coll)))
+          alist-a)
+    (mapc (lambda (b) (push (cons :b (cdr b)) (gethash (car b) coll)))
+          alist-b)
+    (loop :for key :being :the :hash-keys :of coll :using (hash-value val)
+       :collect
+       `(,key
+         ,@(or (cdr (assoc :a val)) missing)
+         ,@(or (cdr (assoc :b val)) missing)))))
+
 (defun top-memory-instances (space &key (top-n 15))
   "Return a list of the TOP-N memory consuming instances in SPACE."
   (mapcar (lambda (line)
@@ -37,7 +50,7 @@
               (multiple-value-bind (whole matches) (scan-to-strings re line)
                 (declare (ignorable whole))
                 (let ((m (coerce matches 'list)))
-                  (cons (car m)
+                  (cons (read-from-string (car m))
                         (mapcar (lambda (l)
                                   (parse-integer (regex-replace-all "," l "")))
                                 (cdr m)))))))
@@ -61,6 +74,14 @@
          (format stream "[~d] ~S~%"
                  (length (sb-impl::string-output-stream-buffer obj))
                  (subseq (sb-impl::string-output-stream-buffer obj)
-                         0 (min 40 (length (sb-impl::string-output-stream-buffer obj)))))))
+                         0 (min 40 (length (sb-impl::string-output-stream-buffer
+                                            obj)))))))
      :dynamic)
     (cons count biggest)))
+
+(defun print-memory ()
+  (sb-ext:gc :full t :force t)
+  (format *standard-output* "~&#|~%")
+  (sb-vm::memory-usage :print-spaces t :print-summary nil)
+  (sb-vm:instance-usage :dynamic :top-n 200)
+  (format *standard-output* "~&|#~%"))
