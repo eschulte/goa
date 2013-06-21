3ddc86d584579a92bce0264be0ceaf0568ee25bf
Eric Schulte <schulte.eric@gmail.com>
2013-06-20 20:06:12 -0600

    applying smoothed annotations to the genome

--- src/annotate.lisp
+++ src/annotate.lisp
@@ -39,34 +39,44 @@
                           (parse-number (aref matches 0))))))
               (split-sequence #\Newline raw)))))
 
-(defun annotate-w-addr (asm &key bin &aux func-addrs)
-  (unwind-protect
-       (let ((my-bin (or bin (phenome asm))))
+(defun genome-addrs (asm &key bin &aux func-addrs)
+  (let ((my-bin (or bin (phenome asm))))
+    (unwind-protect
          (mapcar
           (lambda (l)
             (multiple-value-bind (all matches)
                 (scan-to-strings "^([^\\.][a-zA-Z0-9_]*):" (aget :line l))
               (if all
-                  (setf func-addrs (asm-disassemble my-bin (aref matches 0)))
-                  (when func-addrs
-                    (push (cons :address (pop func-addrs)) l))))
-            l)
-          (genome asm)))
-    (when bin (delete-file bin))))
+                  (prog1 nil
+                    (setf func-addrs (asm-disassemble my-bin (aref matches 0))))
+                  (when func-addrs (pop func-addrs)))))
+          (genome asm))
+      (when (not bin) (delete-file my-bin)))))
 
-(defun annotate-w-perf (asm &key bin)
-  (unwind-protect
-       (let ((my-bin (or bin (phenome asm))))
-         (let ((perf-ann (perf-annotations my-bin)))
-           (format t "perf-ann has ~d~%" (length perf-ann))
-           (mapcar
-            (lambda (l)
-              (when-let ((addr (aget :address l)))
-                (when-let ((anns (aget addr perf-ann)))
-                  (push (cons :annotation anns) l)))
-              l)
-            (annotate-w-addr asm :bin my-bin))))
-    (when bin (delete-file bin))))
+(defun genome-anns (asm &key bin)
+  (let ((my-bin (or bin (phenome asm))))
+    (unwind-protect
+         (mapcar {aget _ (perf-annotations my-bin)}
+                 (genome-addrs asm :bin my-bin))
+      (when (not bin) (delete-file my-bin)))))
+
+(defvar *kernel* '((-3 . 0.006) (-2 . 0.061) (-1 . 0.242)
+                   (0 . 0.383)
+                   (1 . 0.242)  (2 . 0.061) (3 . 0.006)))
+
+(defun smooth (list &optional (kernel *kernel*))
+  "Gaussian smoothing of LIST by KERNEL."
+  (let ((result (make-array (length list) :initial-element 0)))
+    (loop :for el :in list :as i :from 0 :do
+       (loop :for (off . mult) :in kernel :do
+          (let ((ind (+ i off)))
+            (when (and (>= ind 0) (< ind (length list)))
+              (incf (aref result ind) (* mult el))))))
+    (coerce result 'list)))
 
 ;; apply the perf annotations to the genome
-(setf (genome *orig*) (annotate-w-perf *orig*))
+(setf (genome *orig*)
+      (mapcar (lambda (ann element)
+                (cons (cons :annotation ann) element))
+              (smooth (mapcar (lambda (ans) (or ans 0)) (genome-anns *orig*)))
+              (genome *orig*)))
