23b2944c990ce3eab9ebe36b2d71109cbecf0bff
Eric Schulte <schulte.eric@gmail.com>
2013-07-27 11:55:47 -0600

    fix path name in opt-annotate

--- src/opt-annotate.lisp
+++ src/opt-annotate.lisp
@@ -35,4 +35,6 @@
             (cons (cons :annotation ann) element))
           (smooth (mapcar (lambda (ans) (or ans 0)) (genome-anns *orig*)))
           (genome *orig*))
-  (store *orig* (format nil "~a/orig.store" (pathname-name *res-dir*))))
+  (store *orig* (make-pathname :directory *res-dir*
+                               :name "orig"
+                               :type "store")))
